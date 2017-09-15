1aabaab860053a4db67e29bca0c512b075088819
sMishok <shtrucer@list.ru>
2017-09-15 15:45:13 +0300

    JavaGame Lesson 1

---
+++ desktop/src/ru/geekbrains/stargame/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package ru.geekbrains.stargame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.geekbrains.stargame.StarGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1280;
+		config.height = 720;
+		new LwjglApplication(new StarGame(), config);
+	}
+}
